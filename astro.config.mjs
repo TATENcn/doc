ccd6bf31f44386ce1ecb59a907750497e4bdf39e
CN059 <cn059@outlook.com>
2026-02-16 19:51:19 +0800

    fix: 修复打开时404报错

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   site: "http://doc.taten.org",
-  base: "/",
+  base: "/zh-cn/",
   integrations: [
     starlight({
       title: "TATEN Doc",
