be96819920f3f35efeb920318bdfc01aa5afd376
CN059 <cn059@outlook.com>
2026-02-16 19:11:49 +0800

    init: 初始化文档站

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,40 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: "http://doc.taten.org",
+  base: "/",
+  integrations: [
+    starlight({
+      title: "TATEN Doc",
+      description: "TATEN Team Documentation Hub Website",
+      logo: {
+        src: "./src/assets/taten-logo.png",
+        alt: "TATEN Logo",
+      },
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro/starlight",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+        { label: "服务文档", autogenerate: { directory: "services" } },
+      ],
+    }),
+  ],
 });
